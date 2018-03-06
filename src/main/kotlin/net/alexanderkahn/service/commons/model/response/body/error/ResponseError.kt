a843a5dcac2ea3988699320b2255334e45deda19
Alexander Kahn <akahn@jamasoftware.com>
2018-03-06 09:47:29 -0800

    65/address-tech-debt:  Another convenience method

--- src/main/kotlin/net/alexanderkahn/service/commons/model/response/body/error/ResponseError.kt
+++ src/main/kotlin/net/alexanderkahn/service/commons/model/response/body/error/ResponseError.kt
@@ -4,4 +4,5 @@ import net.alexanderkahn.service.commons.model.response.body.meta.ResourceStatus
 
 data class ResponseError(val status: Int, val title: String, val detail: String) {
     constructor(status: ResourceStatus, title: String, detail: String) : this(status.statusCode, title, detail)
+    constructor(status: ResourceStatus, exception: Exception) : this(status.statusCode, status.reasonPhrase, exception.message.orEmpty())
 }
