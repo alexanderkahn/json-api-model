3ed93aa82b470822a4d5b629b29752089af99d1e
Alexander Kahn <akahn@jamasoftware.com>
2018-03-08 11:11:56 -0800

    65/address-tech-debt:  Add property order to ResponseError. Move property order tests from longball-service repo.

--- src/main/kotlin/net/alexanderkahn/service/commons/model/response/body/error/ResponseError.kt
+++ src/main/kotlin/net/alexanderkahn/service/commons/model/response/body/error/ResponseError.kt
@@ -1,7 +1,9 @@
 package net.alexanderkahn.service.commons.model.response.body.error
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder
 import net.alexanderkahn.service.commons.model.response.body.meta.ResourceStatus
 
+@JsonPropertyOrder("status", "title", "detail")
 data class ResponseError(val status: ResourceStatus, val title: String, val detail: String) {
     constructor(status: ResourceStatus, exception: Exception) : this(status, status.defaultReasonPhrase(), exception.message.orEmpty())
 }
